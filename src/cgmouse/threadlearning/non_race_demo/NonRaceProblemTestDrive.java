bb4a080ce01f34b344b3b2ec349ca0a416d387c4
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-25 14:47:57 +0700

    added NonRaceProblemTestDrive for running

---
+++ src/cgmouse/threadlearning/non_race_demo/NonRaceProblemTestDrive.java
@@ -0,0 +1,9 @@
+package cgmouse.threadlearning.non_race_demo;
+
+public class NonRaceProblemTestDrive {
+    public static void main(String[] args) {
+        
+        NonRaceProblem nonRaceProblem = new NonRaceProblem();
+    }
+    
+}
