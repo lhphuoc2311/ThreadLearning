023d2d1b6a5d9ad09ae3c7ba19abcc8b6159124e
Code Guru Mouse <phuoc2311@gmail.com>
2021-08-25 15:08:15 +0700

    added Consumer.java

---
+++ src/cgmouse/threadlearning/race_condition_problem/Consumer.java
@@ -0,0 +1,27 @@
+package cgmouse.threadlearning.race_condition_problem;
+
+public class Consumer extends Thread {
+
+    Store store = null;
+
+    public Consumer(Store s){
+        store = s;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                long x = store.get();
+                if(x > 0){
+                    System.out.println("-- Product " + x + "is bought.");
+                }else{
+                    System.out.println("Consumer is waiting fo new product");
+                }
+            }catch (Exception e){
+
+            }
+        }
+    }
+    
+}
